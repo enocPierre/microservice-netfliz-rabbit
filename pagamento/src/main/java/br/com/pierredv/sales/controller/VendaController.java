d395fa819610c2feb00753d557b46ad6d86240af
enocPierre <enoc.pierre@mercadolivre.com>
2021-12-29 20:04:07 -0300

    add venda controller

--- pagamento/src/main/java/br/com/pierredv/sales/controller/VendaController.java
+++ pagamento/src/main/java/br/com/pierredv/sales/controller/VendaController.java
@@ -1,5 +1,93 @@
 package br.com.pierredv.sales.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pierredv.sales.services.VendaService;
+import br.com.pierredv.sales.vo.VendaVO;
+
+@RestController
+@RequestMapping("/vendas")
 public class VendaController {
+	
+	@Autowired
+	private VendaService vendaService;
+	
+	private final PagedResourcesAssembler<VendaVO> assembler;
+
+	public VendaController(VendaService produtoService, PagedResourcesAssembler<VendaVO> assembler) {
+		super();
+		this.vendaService = produtoService;
+		this.assembler = assembler;
+	}
+
+	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
+	public VendaVO findById(@PathVariable("id") Long id) {
+		VendaVO produtoVO = vendaService.findById(id);
+		produtoVO.add(linkTo(methodOn(VendaController.class).findById(id)).withSelfRel());
+		return produtoVO;
+	}
+	
+	@GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
+	public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "12") int limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "data"));
+		Page<VendaVO> produtos = vendaService.findAll(pageable);
+		
+		produtos.stream()
+				.forEach(p -> p.add(linkTo(methodOn(VendaController.class).findById(p.getId())).withSelfRel()));
+		PagedModel<EntityModel<VendaVO>> pagedModel = assembler.toModel(produtos);
+		
+		return new ResponseEntity<>(pagedModel,HttpStatus.OK);
+	}
+	
+
+	@PostMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+				consumes = {"application/json", "application/xml", "application/x-yaml"})
+	public VendaVO create(@RequestBody VendaVO produtoVO) {
+		VendaVO proVo = vendaService.create(produtoVO);
+		proVo.add(linkTo(methodOn(VendaController.class).findById(proVo.getId())).withSelfRel());
+		return proVo;
+		
+	}
+	
+	@PutMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+			consumes = {"application/json", "application/xml", "application/x-yaml"})
+public VendaVO update(@RequestBody VendaVO vendaVO) {
+	VendaVO proVo = vendaService.upadte(vendaVO);
+	proVo.add(linkTo(methodOn(VendaController.class).findById(vendaVO.getId())).withSelfRel());
+	return proVo;
+    
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		vendaService.delete(id);
+		return ResponseEntity.ok().build();
+	}
 
 }
