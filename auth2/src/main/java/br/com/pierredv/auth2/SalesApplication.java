b4be168409942ee10a8285acbbfb60a0ce8297db
enocPierre <enoc.pierre@mercadolivre.com>
2022-01-05 18:10:23 -0300

    add user, roles and dtos

--- auth2/src/main/java/br/com/pierredv/auth2/SalesApplication.java
+++ auth2/src/main/java/br/com/pierredv/auth2/SalesApplication.java
@@ -1,4 +1,4 @@
-package br.com.pierredv.sales;
+package br.com.pierredv.auth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
